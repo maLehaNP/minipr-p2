2ee2356c35708db19bd67e41abea53e51e2c8735
maLeha <maLehaNP@users.noreply.github.com>
2024-04-23 00:15:48 +0400

    final

--- main.py
+++ main.py
@@ -9,29 +9,20 @@ class MyWidget(QWidget, Ui_Form):
         super().__init__()
         self.setupUi(self)
 
-        # локальный список с аргументами
-        self.args = []
-
-        # загрузка сохраненных значений
-        # with open('items.json', 'r') as argr:
-        #     jlarg = json.load(argr)
-        #     self.yes_lbl.setText(str(jlarg[0]))
-        #     self.no_lbl.setText(str(jlarg[1]))
-
-        # загрузка списка и таблицы таблицы
+        # загрузка списка и таблицы
         with open('items.json') as items:
             self.loc_items = json.load(items)
             for row in self.loc_items:
                 self.rowmaker(row)
 
-        # кнопки
+        # подключение кнопок
         self.yes_btn.clicked.connect(self.buttons)
         self.no_btn.clicked.connect(self.buttons)
         self.safe_btn.clicked.connect(self.buttons)
         self.reset_btn.clicked.connect(self.buttons)
         self.apply_btn.clicked.connect(self.buttons)
         self.ok_btn.clicked.connect(self.buttons)
-        self.add_btn.clicked.connect(self.buttons())
+        self.add_btn.clicked.connect(self.buttons)
 
     # функция кнопок
     def buttons(self):
@@ -40,16 +31,22 @@ class MyWidget(QWidget, Ui_Form):
         if self.sender() == self.no_btn:
             self.no_lbl.setText(f"{int(self.no_lbl.text()) + 1}")
         if self.sender() == self.add_btn:
-            self.loc_items = self.loc_items + [[self.q_lbl.text(), self.yes_lbl.text(), self.no_lbl.text()]]
+            new_item = [self.q_lbl.text(), self.yes_lbl.text(), self.no_lbl.text()]
+            self.loc_items = self.loc_items + [new_item]
+            self.rowmaker(new_item)
+            print(self.loc_items)
         if self.sender() == self.safe_btn:
             self.safe()
         if self.sender() == self.reset_btn:
             self.yes_lbl.setText('0')
             self.no_lbl.setText('0')
-            self.safe()
         if self.sender() == self.ok_btn:
             self.q_lbl.setText(self.q_line.text())
+        if self.sender() == self.apply_btn:
+            self.yes_lbl.setText(self.yes_spin.text())
+            self.no_lbl.setText(self.no_spin.text())
 
+    # рисует строку
     def rowmaker(self, lst):
         rc = self.tableWidget.rowCount()
         self.tableWidget.insertRow(rc)
